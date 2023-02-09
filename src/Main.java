b3b2726212a8d99a9a41af2b1ff76974b8e7f521
Bheemesh <gbheemesh888@gmail.com>
2023-02-09 14:32:23 +0530

    Done...

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,26 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-      /*  System.out.println("Enter your name");
+       System.out.println("Enter your name");
         String name = sc.nextLine();
         System.out.println("Enter your address");
         String address = sc.nextLine();
         System.out.println("Enter amount to be deposited...");
         int amount = sc.nextInt();
         System.out.println("Enter the Password to set your Account....");
-        String password = sc.next(); */
+        String password = sc.next();
 
-        SBIAccount s = new SBIAccount("Bheemesh","ieeja",2000,"Bheemesh");
+        SBIAccount s = new SBIAccount(name,address,amount,password);
         System.out.println(s.getName());
         System.out.println(s.getAccountnumber());
         System.out.println(s.getAddress());
+        //check balane
         System.out.println(s.checkBalance());
+        //add amount and check balance
+        System.out.println(s.addBalance());
+       System.out.println(s.checkBalance());
+        //for withdraw
+        System.out.println(s.withDraw());
 
 
     }
